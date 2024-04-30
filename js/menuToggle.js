6616f6f6c572ea002f0ca654552c171afb4f9e2b
mareanovazquez <mareanovazquez@protonmail.com>
2024-04-30 12:42:37 -0300

    pruebas en fixed blur menu

---
+++ js/menuToggle.js
@@ -0,0 +1,32 @@
+const menuToggle = document.getElementById('toggle');
+const menuShow = document.getElementById('menuMobile');
+const menuItems = document.querySelectorAll(".itemMenuMobile");
+const menuItemsRRSS = document.querySelectorAll(".itemMenuRRSS")
+
+function deployMenu() {
+    menuToggle.classList.toggle('on');
+    menuShow.classList.toggle('menuShow');
+}
+
+function closeMenu() {
+    menuToggle.classList.remove("on");
+    menuShow.classList.remove("menuShow");
+}
+
+menuToggle.addEventListener('click', deployMenu);
+
+// Agrega un controlador de eventos a cada elemento del menú para cerrar el menú cuando se hace clic
+menuItems.forEach(item => {
+    item.addEventListener("click", closeMenu);
+});
+
+menuItemsRRSS.forEach(item => {
+    item.addEventListener("click", closeMenu);
+});
+
+window.addEventListener('keydown', (event) => {
+    if (event.code === 'Escape') {
+        menuToggle.classList.remove("on");
+        menuShow.classList.remove("menuShow");
+    }
+  });
